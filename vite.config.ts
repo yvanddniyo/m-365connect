4190f94a0ff263ea5ac9f3b2ef35a2661db41489
yvanddniyo <nyvan80076@gmail.com>
2025-04-08 22:59:20 +0200

    First commit for project of the m365connect

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 
